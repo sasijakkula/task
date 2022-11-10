9ac2e6e762a70d54308cd6d2bc4edd379df43738
sasijakkula <117454218+sasijakkula@users.noreply.github.com>
2022-11-10 16:39:57 +0530

    task added

---
+++ 1task com.py
@@ -0,0 +1,181 @@
+# problem1: 
+# create a list, value range 30-70
+sas=[]
+for p in range(30,71):
+    print(p)
+    sas.append(p)
+    print(sas)
+
+# create a tuple,value range 70-120
+lst=[]
+for k in range(70,121):
+    print(k)
+    lst.append(k)
+    print(lst)
+    tup=tuple(lst)
+    print(tup)
+# create a set, value range 130-170
+name=[]
+for h in range(130,171):
+    print(h)
+    name.append(h)
+    print(h)
+    pra=set(name)
+    print(pra)
+
+
+    print(n)
+    son.append(n)
+    print(n)
+   set=set{
+    print{set}
+    
+    
+
+
+# problem2:
+# create a list, values- month(january,feburary etc) name 
+months='January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December'
+lst_months=[]
+for u in months:
+    print(u)
+    lst_months.append(u)
+    print(lst_months)
+    
+    
+    
+    
+    
+    
+     
+# create a tuple,value with week(sunday,monday etc) names
+weeks='Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'
+lst_weeks=[]
+for n in weeks:
+    print(n)
+    lst_weeks.append(n)
+    kiran=tuple(lst_weeks)
+    print(kiran)
+
+
+
+
+
+
+
+
+
+ 
+# create a set, value with season(winter,summer etc) names 
+season='SPRING','SUMMER','AUTUMN','WINTER'
+lst_season=[]
+for l in season:
+    print(l)
+    lst_season.append(l)
+    luck=set(lst_season)
+    print(luck)
+
+
+
+
+# problem3: 
+# create a list, values- India all state names 
+state='Arunachal', 'Pradesh','Assam','Bihar','Chhattisgarh','Goa','Gujarat','Haryana','Himachal',' Pradesh','Jharkhand','Karnataka','Kerala','Madhya',' Pradesh'
+lst_state=[]
+for g in state:
+    print(g)
+    lst_state.append(g)
+    print(lst_state)
+
+
+
+# create a tuple,values- color names
+color='red','orange','yellow','green','blue','violet','purple','pink','silver','gold','brown','grey','black','white'
+lst_color=[]
+for c in color:
+    print(c)
+    lst_color.append(c)
+    joy=tuple(lst_color)
+    print(joy)
+
+
+# create a set, values- flower names
+flower='Rose','Lotus','Jasmine','Sunflower','Daisy','Tulip','Magnolia','Lavender','Balsam','Flax',' Pea','Crossandra','Golden',' Shower','Forest'
+lst_flower=[]
+for a in flower:
+    print(a)
+    lst_flower.append(a)
+    nun=set(lst_flower)
+    print(nun)
+
+
+# problem4:
+# P4Q1: 
+# input=["Jan","Feb","Mar","Apr","Maye"]
+# Maye replace with May
+# output=["Jan","Feb","Mar","Apr","May"]
+prbm4=["Jan","Feb","Mar","Apr","Maye"]
+prbm4[4]="may"
+prbm4
+
+# P4Q2: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["Jan","Feb"]
+john=["Jan","Feb","Mar","Apr","May"]
+john[0:2]
+
+
+
+# P4Q3: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["Feb","Mar","Apr"]
+life=["Jan","Feb","Mar","Apr","May"]
+life[1:4]
+
+
+# P4Q4: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["Apr"]
+anu=["Jan","Feb","Mar","Apr","May"]
+anu[3]
+
+
+
+# P4Q5: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["Jan"]
+jony=["Jan","Feb","Mar","Apr","May"]
+jony[0]
+
+
+
+# P4Q6: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["May"]
+sri=["Jan","Feb","Mar","Apr","May"]
+sri[4]
+
+
+
+# problem5:
+# P5Q1: 
+# input=["Jan","Feb","Mar","Apr",Apr","Maye"]
+# output=["Jan","Feb","Mar","Apr","Maye"]
+srav={"Jan","Feb","Mar","Apr","Apr","Maye"}
+srav
+
+
+
+# P5Q2: 
+# input=["Jan","Feb","Feb","Feb","Mar","Apr","May"]
+# output=["Jan","Feb"]
+sony=["Jan","Feb","Feb","Feb","Mar","Apr","May"]
+sony[0:2]
+
+
+
+# P5Q3: 
+# input=["Jan","Feb","Mar","Feb","Mar","Feb","Mar","Apr","May"]
+# output=["Feb","Mar","Apr"]
+mouni=["Jan","Feb","Mar","Feb","Mar","Feb","Mar","Apr","May"]
+mouni[5:8]
