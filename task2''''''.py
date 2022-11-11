d4483e79ecb205b69470a98604ccaf13bdd9cd04
sasijakkula <117454218+sasijakkula@users.noreply.github.com>
2022-11-11 14:15:28 +0530

    task2''''

---
+++ task2''''''.py
@@ -0,0 +1,101 @@
+#prob1:
+#student_roll_number=[121,131,141,151]
+#create a list as student_roll_number_updated
+#student_roll_number_updated=[221,231,241,251]
+lst=[]
+
+student_roll_number=[121,131,141,151]
+for k in student_roll_number:
+    var=k+100
+    print(var)
+    lst.append(var)
+    print("student_roll_number_updated=",lst)
+    
+    
+    
+
+
+
+#prob2:
+#student_roll_number=[121,131,141,151]
+#create a list as last_student_roll_number
+#last_student_roll_number=[251]
+student_roll_number1=[121,131,141,151]
+for s in student_roll_number1:
+    list=[]
+    var3=s+100
+    list.append(var3)
+list
+
+
+
+
+#prob3:
+#create a list as automated_list
+#student_roll_number_updated=[10,20,30,40,50,60,70,80,90]
+lst1=[]
+for m in range(10,91,10):
+    print(m)
+    lst1.append(m)
+    print("student_roll_number_updated", lst1)
+
+
+
+
+
+
+
+#prob4:
+#student_names=["ajay","ravi", "kiranmai","tanuja","hamida"]
+#display all names like "The student is ajay" like that
+lst=["ajay","ravi", "kiranmai","tanuja","hamida"]
+for p in lst:
+    print(" The student is " ,p)
+
+
+
+
+
+#prob5:
+#student_names=["ajay","ravi", "kiranmai","tanuja","hamida"]
+#create a list as student_names_updated 
+
+#student_names_updated=["ajay 10thclass","ravi 10thclass", "kiranmai 10thclass","tanuja 10thclass","hamida 10thclass"]
+lst2=[]
+lst=["ajay","ravi", "kiranmai","tanuja","hamida"]
+for j in lst:
+    var1=j+ "10thclass"
+    lst2.append(var1)
+    print(lst2)
+
+
+
+
+#prob6:
+#student_names=["ajay","ravi", "kiranmai","tanuja","hamida"]
+#create a list as last_student_names
+#last_student_names=["hamida"]
+student_names=["ajay","ravi", "kiranmai","tanuja","hamida"]
+for e in student_names:
+    lst6=[]
+    lst6.append(e)
+lst6
+   
+    
+  
+    
+    
+    
+    
+     
+
+
+
+
+
+
+
+
+
+ 
+ 
