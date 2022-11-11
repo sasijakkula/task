f732d057ecfcc2915593795e9c2423d6a1357abc
sasijakkula <117454218+sasijakkula@users.noreply.github.com>
2022-11-11 14:06:25 +0530

    task2

---
+++ task2.py
@@ -0,0 +1,82 @@
+# problem1: 
+# create a list, value range 30-70
+sas=[]
+for p in range(30,71):
+    print(p)
+    sas.append(p)
+    print(sas)
+
+# create a tuple,value range 70-120
+lst=[]
+for k in range(70,121):
+    print(k)
+    lst.append(k)
+    print(lst)
+    tup=tuple(lst)
+    print(tup)
+# create a set, value range 130-170
+son=[]
+for n in range(130,171):
+    print(n)
+    
+
+
+# problem2:
+# create a list, values- month(january,feburary etc) name 
+months='january','february','march'
+lst_months=[]
+for s in months:
+    print(s)
+    lst_months.append(s)
+    print(lst_months)
+# create a tuple,value with week(sunday,monday etc) names 
+# create a set, value with season(winter,summer etc) names 
+
+# problem3: 
+# create a list, values- India all state names 
+# create a tuple,values- color names
+# create a set, values- flower names
+
+# problem4:
+# P4Q1: 
+# input=["Jan","Feb","Mar","Apr","Maye"]
+# Maye replace with May
+# output=["Jan","Feb","Mar","Apr","May"]
+prbm4=["Jan","Feb","Mar","Apr","Maye"]
+prbm4[4]="may"
+prbm4
+
+# P4Q2: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["Jan","Feb"]
+
+# P4Q3: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["Feb","Mar","Apr"]
+
+# P4Q4: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["Apr"]
+
+# P4Q5: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["Jan"]
+
+# P4Q6: 
+# input=["Jan","Feb","Mar","Apr","May"]
+# output=["May"]
+
+
+
+# problem5:
+# P5Q1: 
+# input=["Jan","Feb","Mar","Apr",Apr","Maye"]
+# output=["Jan","Feb","Mar","Apr","Maye"]
+
+# P5Q2: 
+# input=["Jan","Feb","Feb","Feb","Mar","Apr","May"]
+# output=["Jan","Feb"]
+
+# P5Q3: 
+# input=["Jan","Feb","Mar","Feb","Mar","Feb","Mar","Apr","May"]
+# output=["Feb","Mar","Apr"]
